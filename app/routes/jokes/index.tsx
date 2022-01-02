707cf68225efc9f30bd9d20808839293608acbc1
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2022-01-02 09:59:20 +0530

    shown random joke from db

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,35 @@
+import { Link, LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+  id: string;
+  name: string;
+  content: string;
+};
+
+const getRandomJoke = async (): Promise<LoaderData> => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+    select: { id: true, name: true, content: true },
+  });
+  return randomJoke;
+};
+
+export const loader: LoaderFunction = async () => {
+  const randomjoke = await getRandomJoke();
+  return randomjoke;
+};
+
 export default function JokesIndexRoute() {
+  const joke = useLoaderData<LoaderData>();
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{joke.content}</p>
+      <Link to={`/jokes/${joke.id}`}>{joke.name} Permalink</Link>
     </div>
   );
 }
