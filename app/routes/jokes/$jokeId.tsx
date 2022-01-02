a0c67892fd0bbe6b34b463785441b98e7ff2e512
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2022-01-02 09:59:04 +0530

    shown joke matching id from db

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,29 @@
+import { Link, LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+  name: string;
+  content: string;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { jokeId } = params;
+  const joke = await db.joke.findUnique({
+    where: { id: jokeId },
+    select: { name: true, content: true },
+  });
+  if (!joke) throw new Error("Joke not found");
+  return joke as LoaderData;
+};
+
 export default function JokeRoute() {
+  const joke = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{joke.content}</p>
+      <Link to=".">{joke.name} Permalink</Link>
     </div>
   );
 }
