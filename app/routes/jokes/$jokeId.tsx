426e65530da032ea7b41ffe2ac7794d5f7820d19
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2022-01-07 12:24:02 +0530

    added joke delete function.

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,37 +1,76 @@
+import { Joke } from "@prisma/client";
 import {
+  ActionFunction,
   Link,
   LoaderFunction,
+  redirect,
   useCatch,
   useLoaderData,
   useParams,
 } from "remix";
 import { db } from "~/utils/db.server";
+import { getUserId } from "~/utils/session.server";
 
 type LoaderData = {
-  name: string;
-  content: string;
+  joke: Joke;
+  isOwner: boolean;
 };
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ params, request }) => {
   const { jokeId } = params;
   const joke = await db.joke.findUnique({
     where: { id: jokeId },
-    select: { name: true, content: true },
   });
+  const userId = await getUserId(request);
   if (!joke) {
     throw new Response("What a Joke! Not found.", { status: 404 });
   }
-  return joke as LoaderData;
+  return { joke, isOwner: joke.jokesterId === userId } as LoaderData;
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const form = await request.formData();
+
+  if (form.get("_method") === "delete") {
+    const userId = await getUserId(request);
+    const joke = await db.joke.findUnique({
+      where: { id: params.jokeId },
+    });
+
+    if (!joke) {
+      throw new Response("Can't delete a joke that does not exist", {
+        status: 404,
+      });
+    }
+
+    if (joke.jokesterId !== userId) {
+      throw new Response("That is not your joke.", { status: 401 });
+    }
+
+    await db.joke.delete({
+      where: { id: params.jokeId },
+    });
+
+    return redirect("/jokes");
+  }
 };
 
 export default function JokeRoute() {
-  const joke = useLoaderData<LoaderData>();
+  const { joke, isOwner } = useLoaderData<LoaderData>();
 
   return (
     <div>
       <p>Here's your hilarious joke:</p>
       <p>{joke.content}</p>
       <Link to=".">{joke.name} Permalink</Link>
+      {isOwner && (
+        <form method="post">
+          <input type="hidden" name="_method" value="delete" />
+          <button type="submit" className="button">
+            Delete
+          </button>
+        </form>
+      )}
     </div>
   );
 }
@@ -44,6 +83,14 @@ export function CatchBoundary() {
     return (
       <div className="error-container">
         Huh? What the heck is {params.jokeId}
+      </div>
+    );
+  }
+
+  if (caught.status === 401) {
+    return (
+      <div className="error-container">
+        Sorry, but {params.jokeId} is not your joke.
       </div>
     );
   }
